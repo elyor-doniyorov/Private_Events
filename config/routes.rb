7996d6a42de7aeec2246a2e42ae2c6a8f1188fab
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-19 19:14:57 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
+  resources :users
+  resources :events do
+  resources :invites
+  end
+  get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
